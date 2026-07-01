eb8b3683455ce00b0b7ed2946c9e59cb8b4a1864
Morten <morstr@gmail.com>
2026-07-01 21:44:52 +0200

    Enable Supabase config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 // Unpissed runtime config.
 // Safe to commit when values are public anon values. Never put service_role keys here.
 window.UNPISSED_CONFIG = {
-  ENABLE_SUPABASE: false,
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
+  ENABLE_SUPABASE: true,
+  SUPABASE_URL: 'https://qvccjnqxhwgvzkbnhbdr.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_lsWcvpsm77FxLysfZqlZaA_O0Lti429',
   SUPABASE_STORAGE_BUCKET: 'bathroom-photos'
 };
